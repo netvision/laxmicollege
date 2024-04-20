a22f791b1f1a659181d863e801430db92a34d144
Rakesh Jangid <rakesh@jangid.co.in>
2024-04-20 06:47:33 +0530

    instialised from gsscollege

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,13 @@
-<script setup lang="ts">
-	import { useStore } from '@/store';
-	import maa from '@/assets/maa-gss.jpg';
+<script setup>
 	useHead({
 		title: 'GSS Girls PG College, Chirawa',
 	});
-
-	const store = useStore();
 </script>
 
 <template>
 	<div class="grid place-items-center min-h-[80vh]">
 		<div>
-			<img :src="maa" width="300" class="mx-auto" />
-			<h2 class="py-5 text-3xl my-3">GSS Girls (PG) College, Chirawa</h2>
+			<h2 class="py-5 text-3xl my-3">Laxmi Co-Education PG College</h2>
 		</div>
 	</div>
 </template>
